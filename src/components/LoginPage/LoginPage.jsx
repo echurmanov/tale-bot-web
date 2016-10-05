bd4043fdbaddfe862d8ffc893b0faeb005c5d5fb
Eugene Churmanov <echurmanov@gmail.com>
2016-10-05 16:36:35 +0500

    Добавлена форма входа-регистрации

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,4 +1,6 @@
 import React, { PropTypes, Component } from 'react';
+import { Link } from 'react-router';
+import LinkContainer from 'react-router-bootstrap/lib/LinkContainer';
 
 import './LoginPage.css';
 
@@ -14,8 +16,6 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-
-
     this.state = {
 
     };
@@ -24,10 +24,24 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1>Hello World!</h1>
-
-        <div>
-         
+        <div className='container'>
+          <form className='form-signin'>
+            <h2 className='form-signin-heading'>Войдите</h2>
+            <label htmlFor='inputEmail' className='sr-only'>Email</label>
+            <input type='email' id='inputEmail' className='form-control'
+              placeholder='Email' required='' autoFocus=''
+            />
+            <label htmlFor='inputPassword' className='sr-only'>Пароль</label>
+            <input type='password' id='inputPassword' className='form-control'
+              placeholder='Пароль' required=''
+            />
+            <div className='checkbox'>
+              <label>
+                <input type='checkbox' value='remember-me'/> Запомнить меня
+              </label>
+            </div>
+            <button className='btn btn-lg btn-primary btn-block' type='submit'>Вход/Регистрация</button>
+          </form>
         </div>
       </div>
     );
--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,10 @@ import { IndexRoute, Route }  from 'react-router';
 import App from 'components/App';
 import LoginPage from 'components/LoginPage';
 
+
 export default (
   <Route component={App} path='/'>
     <IndexRoute component={LoginPage}/>
+    <Route component={LoginPage} path='login'/>
   </Route>
 );
